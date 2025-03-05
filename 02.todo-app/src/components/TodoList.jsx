48241d1526f52df89ef451789837be9fb1368fe1
seungjun <sbs2469@naver.com>
2025-03-05 16:13:14 +0900

    02.todo-app | item rendering 구현

--- 02.todo-app/src/components/TodoList.jsx
+++ 02.todo-app/src/components/TodoList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import './TodoList.css'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos}) => {
+  function returnDday(day) {
+    return Math.round(new Date(day.end).getTime() - new Date(day.start).getTime()) / (1000*60*60*24)
+  }
+
   return (
     <ul>
       {todos.map((todo) => {
-        <li key={todo.id}>{todo.title}</li>;
+        return (
+          <li key={todo.idx} className="item mb-3">
+            <div className={todo.category ? `${todo.color} item-body` : 'item-body'}>
+              <h2 className="fs-3 mb-2">{todo.title}</h2>
+              <p className="text-secondary fs-2">{todo.desc}</p>
+            </div>
+              <div className="item-footer fs-1"><span>D - {returnDday(todo)} </span></div>
+          </li> 
+        )
       })}
     </ul>
   );
