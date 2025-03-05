6ab6b883d709e04c154b22051fd14ed43cc2c672
seungjun <sbs2469@naver.com>
2025-03-05 16:14:07 +0900

    02.todo-app | input category 추가

--- 02.todo-app/src/components/TodoInput.jsx
+++ 02.todo-app/src/components/TodoInput.jsx
@@ -1,27 +1,39 @@
 import React from "react";
+import './TodoInput.css'
 
-const TodoInput = ({title, description, startDate, endDate, onTitleChange, onDescriptionChange, onStartChange, onEndChange, handleSubmit}) => {
+const TodoInput = ({formData, btnRef, onChange, onClick, handleSubmit}) => {
   return (
-    <form id="input_form" onSubmit={handleSubmit}>
+    <>
+    <div className="d-flex justify-content-center gap-2">
+            <button className="catBtn" data-color='cat-etc' ref={el => btnRef.current[0] = el} onClick={onClick}>etc</button>
+            <button className="catBtn" data-color='cat-meet' ref={el => btnRef.current[1] = el} onClick={onClick}>meeting</button>
+            <button className="catBtn" data-color='cat-free' ref={el => btnRef.current[2] = el} onClick={onClick}>free time</button>
+            <button className="catBtn" data-color='cat-travel' ref={el => btnRef.current[3] = el} onClick={onClick}>travel</button>
+          </div>
+    <form id="input_form" className="mt-3" onSubmit={handleSubmit}>
       <fieldset>
         <legend></legend>
-        <div className="d-flex flex-column gap-2">
-          <input type="text" placeholder="Task Title" value={title} onChange={(e) => onTitleChange(e.target.value)} />
-          <input type="text" placeholder="Add a description" value={description} onChange={(e) => onDescriptionChange(e.target.value)} />
+        <div className="d-flex flex-column gap-3">
+        
+          <div className="d-flex flex-column gap-2">
+            <input type="text" placeholder="Task Title" name="title" value={formData.title} onChange={onChange} />
+            <input type="text" placeholder="Add a description" name="desc" value={formData.desc} onChange={onChange} />
+          </div>
           <div className="d-flex  gap-5">
             <div className="d-flex flex-column flex-1">
                 <label htmlFor="start-date">시작일</label>
-                <input type="date" id="start-date" value={startDate} onChange={(e) => onStartChange(e.target.value)} />
+                <input type="date" id="start-date" name="start" value={formData.start} onChange={onChange} />
             </div>
             <div className="d-flex flex-column flex-1">
                 <label htmlFor="end-date">종료일</label>
-                <input type="date" id="end-date" value={endDate} onChange={(e) => onEndChange(e.target.value)}/>
+                <input type="date" id="end-date" name="end" value={formData.end} onChange={onChange}/>
             </div>
           </div>
-          <input type="submit" className="btn" value="등록"  />
+          <input type="submit" className="btn btn-color" value="등록"  />
         </div>
       </fieldset>
     </form>
+    </>
   );
 };
 
