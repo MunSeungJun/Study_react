8d376c3cf30f2d5130c0db3be73a9fcf27223acb
seungjun <sbs2469@naver.com>
2025-03-05 16:45:52 +0900

    vite.config.js base 삭제

--- 02.todo-app/vite.config.js
+++ 02.todo-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/Study-react/02.todo-app'
+  plugins: [react()]
+ 
 })
