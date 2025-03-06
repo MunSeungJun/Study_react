12f0560b4adb06d098dc1583603914e83a423d96
seungjun <sbs2469@naver.com>
2025-03-06 10:19:30 +0900

    02.todo-app | item del-btn 추가

--- 02.todo-app/src/components/TodoList.jsx
+++ 02.todo-app/src/components/TodoList.jsx
@@ -1,23 +1,37 @@
 import React from "react";
-import './TodoList.css'
+import "./TodoList.css";
 
-const TodoList = ({ todos}) => {
+const TodoList = ({ todos, onDelete }) => {
   function returnDday(day) {
-    return Math.round(new Date(day.end).getTime() - new Date(day.start).getTime()) / (1000*60*60*24)
+    return (
+      Math.round(new Date(day.end).getTime() - new Date(day.start).getTime()) /
+      (1000 * 60 * 60 * 24)
+    );
   }
 
   return (
     <ul>
       {todos.map((todo) => {
         return (
-          <li key={todo.idx} className="item mb-3">
-            <div className={todo.category ? `${todo.color} item-body` : 'item-body'}>
-              <h2 className="fs-3 mb-2">{todo.title}</h2>
-              <p className="text-secondary fs-2">{todo.desc}</p>
+          <li key={todo.idx} className="item mb-3 d-flex justify-content-between align-items-center">
+            <div className="">
+              <div
+                className={
+                  todo.category ? `${todo.color} item-body` : "item-body"
+                }
+              >
+                <h2 className="fs-3 mb-2">{todo.title}</h2>
+                <p className="text-secondary fs-2">{todo.desc}</p>
+              </div>
+              <div className="item-footer fs-1">
+                <span>D - {returnDday(todo)} </span>
+              </div>
             </div>
-              <div className="item-footer fs-1"><span>D - {returnDday(todo)} </span></div>
-          </li> 
-        )
+            <div>
+              <button className="del-btn" onClick={() => onDelete(todo.idx)}>&times;</button>
+            </div>
+          </li>
+        );
       })}
     </ul>
   );
