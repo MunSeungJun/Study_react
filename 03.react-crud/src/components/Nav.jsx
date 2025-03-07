2c24231851d7a1f52f71a41f260aaab64dfdfb08
seungjun <sbs2469@naver.com>
2025-03-07 16:21:40 +0900

    03.react-crud | nav component

--- 03.react-crud/src/components/Nav.jsx
+++ 03.react-crud/src/components/Nav.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Trash, Pencil } from 'react-bootstrap-icons';
 
-const Nav = () => {
+const Nav = ({items, onChangeMode, onSelect, onDelete}) => {
   return (
     <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
+        {
+          items.map(item => {
+                  return (
+                    <li className="item d-flex justify-content-between" key={item.id}>
+                      <a href="/" onClick={onChangeMode} data-mode='Read'>{item.title}</a>
+                      <div className="item-body">
+                        <div>
+                          <button onClick={() => onSelect(item.id)}><Pencil/></button>
+                          <button onClick={() => onDelete(item.id)}>
+                            <Trash />
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })
+        }
     </ul>
   )
 }
