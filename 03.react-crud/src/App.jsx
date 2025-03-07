7ae83f12a203f7c54ed50d6f43ca9dc1eec25fd0
seungjun <sbs2469@naver.com>
2025-03-07 16:23:30 +0900

    03.react-crud | 비동기요청 처리 완료(get, put, delete)

--- 03.react-crud/src/App.jsx
+++ 03.react-crud/src/App.jsx
@@ -1,85 +1,139 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Article from './components/Article';
-import Contents from './components/Contents';
-import Lists from './components/Lists';
-import Popup from './components/Popup';
+import Create from './components/Create';
+import Update from './components/Update';
+import axios from 'axios';
 import './components/App.css';
 
 const App = () => {
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get('http://localhost:3000/items');
+        setItems(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+  }, []);
+  const [mode, setMode] = useState('Welcome');
   const [items, setItems] = useState([]);
-  const [id, setId] = useState(1);
+  const [idx, setIdx] = useState(1);
   const [formData, setFormData] = useState({
     title: '',
     body: '',
   });
-  const popupRef = useRef('')
-  const [updateData, setupdateData] = useState({
-    idx:'',
-    body:'',
-    title: ''
-  })
+  const [select, setSelect] = useState({
+    id: '',
+    title: '',
+    body: '',
+  });
+  const updateRef = useRef('');
 
+  function onChangeMode(e) {
+    e.preventDefault();
+    setMode(e.target.dataset.mode);
+  }
   function onChange(e) {
     const { name, value } = e.target;
     setFormData(prev => {
       return { ...prev, [name]: value };
     });
   }
-  function onSubmit(e) {
+  function onCreate(e) {
     e.preventDefault();
     setItems(prev => {
-      return [...prev, { ...formData, idx: id }];
+      return [...prev, { ...formData, id: idx }];
     });
-    setId(i => i + 1);
+    axios
+      .post('http://localhost:3000/items', {
+        id: `${idx}`,
+        title: formData.title,
+        body: formData.body,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    setIdx(i => i + 1);
     setFormData({
       title: '',
       body: '',
     });
   }
-  function onUpdate(itemId) {
-    popupRef.current.classList.remove('d-none')
-    setupdateData(prev => {
-      return (
-        {...prev, idx: (items.filter(v => v.idx == itemId)[0]).idx, title:(items.filter(v => v.idx == itemId)[0]).title, body:(items.filter(v => v.idx == itemId)[0]).body  }
-      )
-    }) 
-
-   
+  function onSelect(itemId) {
+    updateRef.current.classList.remove('d-none');
+    const selected = items.filter(v => v.id === itemId)[0];
+    setSelect(prev => {
+      return { ...prev, id: selected.id, body: selected.title, title: selected.body };
+    });
   }
-  function onUpdateChange(e) {
-    const {name, value} = e.target
-    setupdateData(prev => {
-      return (
-        {...prev, [name]: value}
-      )
-    })
-
+  function onSelectChange(e) {
+    const { name, value } = e.target;
+    setSelect(prev => {
+      return { ...prev, [name]: value };
+    });
   }
-  function onUpdateSubmit(e, itemId) {
-    e.preventDefault()
+  function onUpdate(e) {
+    e.preventDefault();
+    const updateTitle = e.target.elements['title'].value;
+    const UpdateBody = e.target.elements['body'].value;
     setItems(prev => {
-      return (
-        [...(prev.filter(v => v.idx == itemId)), {title:updateData.title, body:updateData.body}]
-      )
+      return prev.filter(v => v.id !== select.id);
+    });
+    setItems(prev => {
+      return [...prev, { id: select.id, title: updateTitle, body: UpdateBody }];
+    });
+    axios
+    .put(`http://localhost:3000/items/${select.id}`, {
+      title: updateTitle,
+      body: UpdateBody
     })
-    popupRef.current.classList.add('d-none')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    setSelect({
+      id: '',
+      title: '',
+      body: '',
+    });
+    updateRef.current.classList.add('d-none');
   }
+
   function onDelete(itemId) {
     setItems(prev => {
-      return prev.filter(v => v.idx !== itemId)
+      return prev.filter(v => v.id !== itemId);
+    });
+    console.log(itemId);
+    axios
+    .delete(`http://localhost:3000/items/${itemId}`)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
   }
   return (
-    
     <>
-      <Header title="WEB" />
-      <Nav />
-      <Article />
-      <Contents formData={formData} onSubmit={onSubmit} onChange={onChange} />
-      <Lists items={items} onUpdate={onUpdate}  onDelete={onDelete} />
-      <Popup popupRef={popupRef} updateData={updateData} onUpdateChange={onUpdateChange} onUpdateSubmit={onUpdateSubmit} />
+      <Header title="WEB" onChangeMode={onChangeMode} />
+      <Nav items={items} onChangeMode={onChangeMode} onSelect={onSelect} onDelete={onDelete} />
+      <Article select={select} mode={mode} />
+      <Create formData={formData} onCreate={onCreate} onChange={onChange} />
+      <Update
+        updateRef={updateRef}
+        select={select}
+        onUpdate={onUpdate}
+        onSelectChange={onSelectChange}
+      />
     </>
   );
 };
