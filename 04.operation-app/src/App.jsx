35361a54946f815f0a41a7c13eee75097f7c6546
MunSeungJun <sbs2469@naver.com>
2025-03-09 14:30:49 +0900

    04.operation-app | import browser-router

--- 04.operation-app/src/App.jsx
+++ 04.operation-app/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
 import Header from './components/Header'
 import Content from './components/Content'
+import './App.css'
 
 const App = () => {
+
   return (
     <>
-      <Header/>
-      <Content/>
+      <div className='container'>
+        <BrowserRouter>
+          <Header/>
+          <Content/>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
 
-export default App
+export default App 
