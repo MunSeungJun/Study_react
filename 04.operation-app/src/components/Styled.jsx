7ab8421893bf3a4e5fe5e66410ed05602e140ad9
seungjun <sbs2469@naver.com>
2025-03-10 17:53:09 +0900

    04.operation-app | styled components.jsx

---
+++ 04.operation-app/src/components/Styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SpaceTd = styled.td`
+background-color: #f1f1f1;
+padding: .5rem;`
+
+export const StudentBtn = styled.button`
+border: none;
+color: orange;
+background-color: white;
+`;
