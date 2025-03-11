1f951b2381b5f10a8fafdfda022835c7c8f43279
seungjun <sbs2469@naver.com>
2025-03-11 17:55:05 +0900

    08.coinpaprika-app | main.jsx

--- 08.coinpaprika-app/src/main.jsx
+++ 08.coinpaprika-app/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Root from './routes/Root'
+import Home, {Loader as HomeLoader} from './routes/Home'
+import ErrorPage from './ErrorPage'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+        path:'home',
+        element: <Home/>,
+        loader: HomeLoader
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
