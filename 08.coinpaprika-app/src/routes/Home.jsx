624c6277e70d67aceddaa6ae506984d65044a18e
MunSeungJun <sbs2469@naver.com>
2025-03-12 18:00:23 +0900

    08.coinpaprika-app | home component 환율 api 적용

--- 08.coinpaprika-app/src/routes/Home.jsx
+++ 08.coinpaprika-app/src/routes/Home.jsx
@@ -1,49 +1,45 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import axios from 'axios';
+import { getCoin, getExRate, addComma } from '../api.js';
 import './Home.css';
 
 export async function Loader() {
-  const response = await axios.get('https://api.coinpaprika.com/v1/tickers');
-  return JSON.stringify(response.data);
+  const [coins, exRates] = await Promise.all([getCoin(), getExRate()]);
+  return { coins, exRates };
 }
 
 const Home = () => {
-  const datas = useLoaderData();
-  const coins = JSON.parse(datas).filter(v => v.rank < 100);
+  const { coins, exRates } = useLoaderData();
+  const exRate = parseInt(exRates[22].deal_bas_r.replace(/,/g, ''));
   return (
     <>
-        <div className='graph'>
-            <img src="http://localhost:5173/img/graph-img.png" alt="" />
-        </div>
-        <div className="table">
-          <div className="thead">
-            <ul>
-              <li>랭크</li>
-              <li>이름</li>
-              <li>기호</li>
-              <li>현재가</li>
-              <li>총시가</li>
-              <li>거래량(24H)</li>
-              <li>변동(24H)</li>
-            </ul>
-          </div>
-          <div className="tbody">
-            <ul>
-              {coins.map(coin => (
-                <li key={coin.id}>
-                  <div>{coin.rank}</div>
-                    <div>{coin.name}</div>
-                    <div>{coin.symbol}</div>
-                  <div>{coin.quotes.USD.price}</div>
-                  <div>{coin.quotes.USD.market_cap}</div>
-                  <div>{parseInt(coin.quotes.USD.volume_24h)}</div>
-                  <div>{coin.quotes.USD.volume_24h_change_24h}</div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+      <div className="graph">
+        <img src="http://localhost:5173/img/trading-img.png" alt="" />
+      </div>
+      <table className="table">
+        <thead className="thead">
+          <tr>
+            <th>이름</th>
+            <th>기호</th>
+            <th>현재가</th>
+            <th>총시가</th>
+            <th>거래량(24H)</th>
+            <th>변동(24H)</th>
+          </tr>
+        </thead>
+        <tbody className="tbody">
+          {coins.map(coin => (
+            <tr key={coin.id}>
+              <td>{coin.name}</td>
+              <td className='text-center'>{coin.symbol}</td>
+              <td className='text-end'>{addComma(Number((coin.quotes.USD.price * exRate).toFixed(1)))}</td>
+              <td className='text-end'>{parseInt((coin.quotes.USD.market_cap * exRate) / 100000000)}억</td>
+              <td className='text-end'>{parseInt((coin.quotes.USD.volume_24h * exRate) / 100000000)}억</td>
+              <td className='text-end'>{coin.quotes.USD.volume_24h_change_24h}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
