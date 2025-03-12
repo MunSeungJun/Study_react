bd684d416d583f85ee1236ce1242e28816caf825
MunSeungJun <sbs2469@naver.com>
2025-03-12 18:01:20 +0900

    08.coinpaprika-app | 뉴스, 주요뉴스, 속보 route 추가

--- 08.coinpaprika-app/src/main.jsx
+++ 08.coinpaprika-app/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Root from './routes/Root'
 import Home, {Loader as HomeLoader} from './routes/Home'
+import News from './routes/News'
+import HeadLine, {Loader as HeadLineLoader} from './routes/Headline'
+import { Flash } from './routes/Flash'
 import ErrorPage from './ErrorPage'
 import './index.css'
 
@@ -15,7 +18,24 @@ const router = createBrowserRouter([
       {
         path:'home',
         element: <Home/>,
+        // index:true,
         loader: HomeLoader
+      },
+      {
+        path:'news',
+        element: <News/>,
+        children:[
+          {
+            path:'headline',
+            element:<HeadLine/>,
+            loader: HeadLineLoader
+        },
+        {
+          path:'flash',
+          element:<Flash/>,
+          index: true
+        }
+      ]
       }
     ]
   }
