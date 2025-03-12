3a1686b75c7a0a2a6d4b5dd3ea2c291bfa9567db
MunSeungJun <sbs2469@naver.com>
2025-03-12 17:59:30 +0900

    08.coinpaprika-app | 주요뉴스 component( description 수정 필요)

---
+++ 08.coinpaprika-app/src/routes/Headline.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { getHeadLine, changeText } from '../api.js';
+import './HeadLine.css';
+
+export async function Loader() {
+  return JSON.stringify(await getHeadLine());
+}
+
+const Headline = () => {
+  const Data = useLoaderData();
+  const headlines = JSON.parse(Data);
+  return (
+    <ul className="headline-list">
+      {headlines.map(headline => (
+        <li key={headline.article_id} className="headline-item">
+          <div className="hadline-item-img">
+            <img src={headline.image_url} alt="" />
+          </div>
+          <div className="headline-item-body">
+            <p className="headline-item-body-title">{headline.title}</p>
+            <p className="headline-item-body-description">{(headline.description)}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Headline;
