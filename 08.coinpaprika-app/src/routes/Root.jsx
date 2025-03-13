f90610568f89acadab834381b0c9356aa29aec6d
MunSeungJun <sbs2469@naver.com>
2025-03-13 14:35:08 +0900

    08.coinpaprika-app | root 광고 위치 조정 및 헤더,메인 width 변경

--- 08.coinpaprika-app/src/routes/Root.jsx
+++ 08.coinpaprika-app/src/routes/Root.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Outlet, NavLink, Link } from 'react-router-dom';
 import './Root.css';
 
+
+
 const Root = () => {
+  const scrollRef = useRef([]);
+  window.addEventListener('scroll', () => {
+    let scrollY = window.scrollY
+    scrollRef.current[0].style.top = `${scrollY}px`
+    scrollRef.current[1].style.top = `${scrollY}px`
+  })
   return (
     <>
       <div id="header">
@@ -12,22 +20,25 @@ const Root = () => {
             <Link to="/home">홈</Link>
             <Link>통계/지표</Link>
             <Link>시황/분석</Link>
-            <Link to='/news/flash'>뉴스</Link>
+            <Link to="/news/flash">뉴스</Link>
             <Link>내정보</Link>
           </nav>
         </div>
       </div>
-        <div id="main">
-          <div className="main-container">
-            <div className='ad'>
-              <img src="http://localhost:5173/img/ad-img.png" alt=""  />
-            </div>
-            <Outlet />
+      <div id="main" >
+        <div className="main-container">
+          <div className="ad">
+            <img src="http://localhost:5173/img/ad-img.png" alt="" />
           </div>
-        <div id='aside'>
-          <img src="http://localhost:5173/img/side-ad-img.png" alt="" />
+          <Outlet />
+          <div id="aside-left-wrap" ref={el => scrollRef.current[0] = el}>
+              <img src="http://localhost:5173/img/side-ad-img.png" alt="" />
+          </div>
+          <div id="aside-right-wrap" ref={el => scrollRef.current[1] = el} >
+              <img src="http://localhost:5173/img/side-ad-img2.png" alt="" />
+          </div>
         </div>
-        </div>
+      </div>
     </>
   );
 };
