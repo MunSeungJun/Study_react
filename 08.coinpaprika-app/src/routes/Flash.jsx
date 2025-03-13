f4ffe15eab294db4322758d4e3e251f30d73ea0a
MunSeungJun <sbs2469@naver.com>
2025-03-13 15:41:50 +0900

    08.coinpaprika-app | flash router

--- 08.coinpaprika-app/src/routes/Flash.jsx
+++ 08.coinpaprika-app/src/routes/Flash.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-import './Flash.css'
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { getFlash } from '../api.js';
+import './Flash.css';
 
-export const Flash = () => {
-  return (
-    <div>속보임...</div>
-  )
+export async function Loader() {
+  return JSON.stringify(await getFlash());
 }
+
+const Flash = () => {
+  const Data = useLoaderData();
+  const flashes = JSON.parse(Data);
+  return (
+    <ul className="flash-list">
+      {flashes.map(flash => (
+        <li key={flash.article_id} className="flash-item">
+          <div className="flash-item-img">
+            <img src={flash.image_url} alt="" />
+          </div>
+          <div className="flash-item-body">
+            <p className="flash-item-body-title">{flash.title}</p>
+            <p className="flash-item-body-description">{(flash.description)}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Flash;
