9b3e97ac908180963da51d3f034e1717409c7352
MunSeungJun <sbs2469@naver.com>
2025-03-17 17:49:22 +0900

    09.hello-mongoDB | frontend | createData() api 추가

--- 09.hello-mongoDB/frontend/src/api.js
+++ 09.hello-mongoDB/frontend/src/api.js
@@ -1,11 +1,25 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export async function getData() {
-    try{
-        const response = await axios.get('http://localhost:3000')
-        return response.data
-    } catch(err) {
-        console.log(err)
-    }
-    
+  try {
+    const response = await axios.get('http://localhost:3000');
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function createData(data) {
+  axios
+    .post('http://localhost:3000/movie', {
+      title: data.get('title'),
+      body: data.get('body'),
+      poster: data.get('poster'),
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
