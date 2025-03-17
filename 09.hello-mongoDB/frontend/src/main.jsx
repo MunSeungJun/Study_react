d2eac18092dff282a3ca0c42d9641663e591fee6
MunSeungJun <sbs2469@naver.com>
2025-03-17 17:48:33 +0900

    09.hello-mongoDB | frontend | import AppAction

--- 09.hello-mongoDB/frontend/src/main.jsx
+++ 09.hello-mongoDB/frontend/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
-import App, { Loader as AppLoader} from './routes/App.jsx'
+import App, { Loader as AppLoader, Action as AppAction} from './routes/App.jsx'
 
 const router = createBrowserRouter([
   {
     path:'/',
     element:<App/>,
-    loader:AppLoader
+    loader:AppLoader,
+    action:AppAction
   }
 ])
 
