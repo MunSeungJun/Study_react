67326d896c43b203369e7f38f1b2a1c60d3fa5eb
MunSeungJun <sbs2469@naver.com>
2025-03-19 14:14:48 +0900

    09.hello-mongoDB | backend | post요청 처리

--- 09.hello-mongoDB/backend/app.js
+++ 09.hello-mongoDB/backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { readUsers } from "./crud-read.js";
+import { createUsers } from './crud-create.js'
 
 const app = express();
 const port = 3000;
@@ -8,10 +9,15 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/list", async (req, res) => {
   const users = await readUsers();
   res.json(users);
 });
+
+app.post('/add', (req, res) => {
+  createUsers(req.body)
+  res.status(200)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
