783b9e032e4b2557bdbc8b0e2951bf23d3c0705c
MunSeungJun <sbs2469@naver.com>
2025-03-19 17:08:07 +0900

    09.hello-mongoDB | frontend | list, add route 추가

--- 09.hello-mongoDB/frontend/src/main.jsx
+++ 09.hello-mongoDB/frontend/src/main.jsx
@@ -1,20 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import './index.css'
-import App, { Loader as AppLoader, Action as AppAction} from './routes/App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './routes/App.jsx';
+import List, { Loader as ListLoader } from './routes/List.jsx';
+import Add, { Action as AddAction } from './routes/Add.jsx'
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
-    loader:AppLoader,
-    action:AppAction
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/list',
+    element: <List />,
+    loader: ListLoader,
+  },
+  {
+    path:'/add',
+    element:<Add/>,
+    action: AddAction
   }
-])
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
