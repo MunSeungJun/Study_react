a049d2fe5ca4afe9d7623e40a78a2763378b7bd0
MunSeungJun <sbs2469@naver.com>
2025-03-19 14:17:44 +0900

    09.hello-mongoDB | frontend | home Ui

--- 09.hello-mongoDB/frontend/src/routes/App.jsx
+++ 09.hello-mongoDB/frontend/src/routes/App.jsx
@@ -1,50 +1,10 @@
 import React from 'react';
-import { Container, Badge } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { getData, createData } from '../api.js';
-import { useLoaderData, Form } from 'react-router-dom';
-
-export async function Loader() {
-  return JSON.stringify(await getData());
-}
-
-export async function Action({request}) {
-  const data = await request.formData()
-  createData(data)
-  
-}
   
   const App = () => {
-    const data = JSON.parse(useLoaderData());
-
   return (
     <>
-      <Container>
-        <Form method="post" className='border p-3 w-75 mx-auto d-flex flex-column align-items-center'  >
-          <div>
-            <input type="text" name="title" />
-            <input type="text" name="body" />
-          </div>
-          <div className='d-flex gap-3'>
-            <input type="text" name="poster"/>
-            <input type="submit" value="등록" />
-          </div>
-        </Form>
-        <ul className="row mt-4">
-          {data.map((v, i) => (
-            <li key={i} className="col-4">
-              <img src={v.poster} alt="" className="img-fluid" />
-              <div className='d-flex align-items-center gap-2'>
-                <Badge>제목</Badge>
-                <span>{v.title}</span>
-              </div>
-              <p>{v.body}</p>
-            </li>
-          ))}
-        </ul>
-      </Container>
-    </>
-  );
+    <div>사원관리시스템</div></>
+      );
 };
 
 export default App;
