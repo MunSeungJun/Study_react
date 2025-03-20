460eb5790fc09c59be8e8b124e4740170ff07943
MunSeungJun <sbs2469@naver.com>
2025-03-20 17:50:47 +0900

    09.hello-mongoDB | frontend | create common Ui

--- 09.hello-mongoDB/frontend/src/routes/App.jsx
+++ 09.hello-mongoDB/frontend/src/routes/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-  
-  const App = () => {
+import { Outlet, NavLink } from 'react-router-dom';
+import { Container, Row,Col } from 'react-bootstrap';
+
+const App = () => {
   return (
     <>
-    <div>사원관리시스템</div></>
-      );
+      <header className='border-bottom'>
+        <Container>
+          <Row className="p-5">
+              <div className='col-4 fs-1 fw-bold'>WP ERP</div>
+              <nav className='col-8 d-flex align-items-center'>
+                <ul className="fs-5 d-flex gap-5">
+                  <li>
+                    <NavLink to="/">home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/list">list</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/add">add</NavLink>
+                  </li>
+                </ul>
+              </nav>
+          </Row>
+        </Container>
+      </header>
+      <main className='mt-5'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <footer></footer>
+    </>
+  );
 };
 
 export default App;
