ffa062219df063bc10265eb64afe2d69d683f530
MunSeungJun <sbs2469@naver.com>
2025-03-20 17:45:09 +0900

    09.hello-mongoDB | backend | delete 요청 처리

--- 09.hello-mongoDB/backend/app.js
+++ 09.hello-mongoDB/backend/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { readUsers } from "./crud-read.js";
-import { createUsers } from './crud-create.js'
+import { createUsers } from "./crud-create.js";
+import { deleteUsers } from "./crud-delete.js";
 
 const app = express();
 const port = 3000;
@@ -10,13 +11,40 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/list", async (req, res) => {
-  const users = await readUsers();
-  res.json(users);
+  try {
+    const users = await readUsers();
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(501).json({
+      message: "fail",
+    });
+  }
 });
 
-app.post('/add', (req, res) => {
-  createUsers(req.body)
-  res.status(200)
+app.post("/add", async (req, res) => {
+  try {
+    await createUsers(req.body);
+    res.status(200).json({
+      message: "success",
+    });
+  } catch (e) {
+    res.status(501).json({
+      message: "fail",
+    });
+  }
+});
+
+app.delete('/delete/:id', async(req, res) => {
+  try {
+    await deleteUsers({employee_id: req.params.id})
+    res.status(200).json({
+      message:'success'
+    })
+  } catch (e) {
+    res.status(501).json({
+      message: "fail"
+    })
+  }
 })
 
 app.listen(port, () => {
