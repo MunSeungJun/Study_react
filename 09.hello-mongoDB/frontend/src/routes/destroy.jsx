3362282266f7ff841211b22d1b3f628757674abe
MunSeungJun <sbs2469@naver.com>
2025-03-20 17:49:51 +0900

    09.hello-mongoDB | frontend | add destroy route

---
+++ 09.hello-mongoDB/frontend/src/routes/destroy.jsx
@@ -0,0 +1,7 @@
+import { redirect } from 'react-router-dom'
+import { deleteData } from '../api'
+
+export async function Action({params}) {
+    await deleteData(params.id)
+    return redirect('/list')
+}
