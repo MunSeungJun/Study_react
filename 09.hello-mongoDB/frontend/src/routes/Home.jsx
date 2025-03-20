239c57bd11c889c9286151cac28356f730bc4642
MunSeungJun <sbs2469@naver.com>
2025-03-20 17:49:27 +0900

    09.hello-mongoDB | frontend | home route 추가

---
+++ 09.hello-mongoDB/frontend/src/routes/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="text-center mt-5">
+      <h1 className=" fs-1 fw-bold text-capitalize mb-4">
+        supercharge your growing business from your wordpress dashboard
+      </h1>
+      <p>WP ERP optimizes your small to medium businesses with a powerful HR Manager,</p>
+      <p>CRM and Accounting tools - Unlock more with 20+ extension & Project</p>
+      <p>Managemnet module</p>
+      <button className="btn">Get Started</button>
+      <button className="btn">Demo</button>
+    </div>
+  );
+};
+
+export default Home;
