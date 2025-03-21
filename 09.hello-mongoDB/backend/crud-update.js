a8a2a673e635139eadc4705fc08677c435082eed
MunSeungJun <sbs2469@naver.com>
2025-03-21 17:17:54 +0900

    09.hello-mongoDB | backend | create update module

---
+++ 09.hello-mongoDB/backend/crud-update.js
@@ -0,0 +1,17 @@
+import {MongoClient} from "mongodb"
+
+const uri = "mongodb+srv://demouser:demo1234@cluster0.jzdiw.mongodb.net/";
+const client = new MongoClient(uri);
+
+export async function updateUsers(id, data) {
+    console.log(id)
+    console.log(data)
+  try {
+    await client.connect();
+    const db = client.db("hr");
+    const coll = db.collection("employees");
+    coll.updateOne(id, {$set: data})
+  } catch (e) {
+    console.log(e);
+  }
+}
