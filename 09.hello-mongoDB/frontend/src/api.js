af6b99a557c8a0234f18b269b5fbe0c4629af677
MunSeungJun <sbs2469@naver.com>
2025-03-21 17:19:02 +0900

    09.hello-mongoDB | frontend | add getSelectData, updateData api

--- 09.hello-mongoDB/frontend/src/api.js
+++ 09.hello-mongoDB/frontend/src/api.js
@@ -9,10 +9,19 @@ export async function getData() {
   }
 }
 
+export async function getSeletData(id) {
+  try {
+    const response = await axios.get(`http://localhost:3000/list/${id}`);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export async function createData(data) {
   axios
     .post('http://localhost:3000/add', {
-      employee_id: (data.get('employee_id')),
+      employee_id: data.get('employee_id'),
       last_name: data.get('last_name'),
       first_name: data.get('first_name'),
       email: data.get('email'),
@@ -27,6 +36,26 @@ export async function createData(data) {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+export async function updateData(id, data) {
+  axios.put(`http://localhost:3000/update/${id}`,{
+    employee_id: data.get('employee_id'),
+      last_name: data.get('last_name'),
+      first_name: data.get('first_name'),
+      email: data.get('email'),
+      phone_number: data.get('phone_number'),
+      salary: data.get('salary'),
+      commission_pct: data.get('commission_pct'),
+      manager_id: data.get('manager_id')
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
 }
 
 export async function deleteData(id) {
